d7617445556cd7ded9938ebb6f5459c699e5f3d6
rafaeldpsilva <1190979@isep.ipp.pt>
2023-06-13 18:39:01 +0100

    weather

--- peakDemo/agent_weather.py
+++ peakDemo/agent_weather.py
@@ -1,40 +1,49 @@
 # agent_manager.py
 from peak import Agent, OneShotBehaviour, CyclicBehaviour, Message
 from spade.template import Template
-
+import pandas as pd
 
 class agent_weather(Agent):
-    weather_data = {
-            'DateTime': '00:15:00',
-            'Generated power': '0.0',
-            'TemperatureC': '11.0',
-            'DewpointC': '8.0',
-            'PressurehPa': '1021.0',
-            'WindDirectionDegrees': '128.33333333333334',
-            'WindSpeedKMH': '8.666666666666666',
-            'WindSpeedGustKMH': '17.0',
-            'Humidity': '83.0',
-            'HourlyPrecipMM': '0.0',
-            'dailyrainMM': '0.0',
-            'SolarRadiationWatts_m2': '0.0'
-    }
-
     class ReceiveMessage(CyclicBehaviour):
+        async def on_start(self):
+            #Ler ficheiro metromaria.csv
+            self.meteorologia = pd.read_csv("dataset-final.csv")
+            self.last_index = 0
+            
+
         async def run(self):
             msg = await self.receive(10)
             if msg:
-                print("ReceiveMessage")
+                print("Weather - ReceiveMessage")
                 print(f"Weather - {msg.sender} sent me a message: '{msg.body}")
                 weather_report = Message(to=f"agent_manager@{self.agent.jid.domain}/am")
                 weather_report.set_metadata("performative", "inform")
-                weather_report.body = str(agent_weather.weather_data)
+                
+                line = self.meteorologia.iloc[self.last_index]
+                weather_data = {
+                    'DateTime': line['hour'],
+                    'Generated power': line['generated_power'],
+                    'TemperatureC': line['temperatureC'],
+                    'DewpointC': line['dewpointC'],
+                    'PressurehPa': line['pressurehPa'],
+                    'WindDirectionDegrees': line['wind_direction_degrees'],
+                    'WindSpeedKMH': line['wind_speed_KMH'],
+                    'WindSpeedGustKMH': line['wind_speed_gustKMH'],
+                    'Humidity': line['Humidity'],
+                    'HourlyPrecipMM': line['hourly_precipMM'],
+                    'dailyrainMM': line['daily_rainMM'],
+                    'SolarRadiationWatts_m2': line['solar_radiation_Watts_m2']
+                }
+                self.last_index += 1
+                
+                weather_report.body = str(weather_data)
                 await self.send(weather_report)
-                # Request and response?
+                
                 # weather_response = await self.receive(5)
                 # print(weather_response)
-            # await self.agent.stop()
+                # await self.agent.stop()
             else:
-                print("Did not received any message after 10 seconds")
+                print("Weather - Did not received any message after 10 seconds")
 
         async def on_end(self):
             # stop agent from behaviour
