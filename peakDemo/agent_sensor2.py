2985063a3d06e89e5622028be7b0fa7345bd1230
LeticiaGomes1181466 <94696179+LeticiaGomes1181466@users.noreply.github.com>
2023-06-13 18:47:47 +0100

    update Subscribe/Publisher Agents

--- peakDemo/agent_sensor2.py
+++ peakDemo/agent_sensor2.py
@@ -3,29 +3,12 @@ from peak import Agent, OneShotBehaviour, Message, PeriodicBehaviour
 
 from paho.mqtt import client as mqtt_client
 import time
+import sensorSubscriber
 BROKER = 'broker.emqx.io'
 PORT = 1883
 TOPIC = "report/agent2"
-# Generate a Client ID with the subscribe prefix.
 CLIEND_ID = f'subscribe-agent2'
-# username = 'emqx'
-# password = 'public'
-# CLIENT=None
 last_reading=None
-
-def connect_mqtt(self) -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(CLIEND_ID)
-    # client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(BROKER, PORT)
-    return client
-
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -35,21 +18,20 @@ def on_message(client, userdata, msg):
 class agent_sensor2(Agent):
     class SendSensorData(PeriodicBehaviour):
         async def run(self):
-            while self.client.loop() == 0:
-                time.sleep(1) 
-                if last_reading is not None:
-                    #random_float = random.uniform(1, 10)
-                    msg = Message(to=f"agent_manager@{self.agent.jid.domain}/am")
-                    msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
-                    msg.body = "Sensor2-%f" % last_reading
-                    # print("My own id",self.agent.jid)
-                    await self.send(msg)
-                    # await self.agent.stop()
+            self.agent.client.loop()
+            time.sleep(1) 
+            if last_reading is not None:
+                msg = Message(to=f"agent_manager@{self.agent.jid.domain}/am")
+                msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+                msg.body = "Sensor2-"+str(last_reading)
+                await self.send(msg)
 
     async def setup(self):
         period = 1
-        self.client = connect_mqtt()
-        self.client.subscribe(TOPIC)
-        self.client.on_message = on_message
         behavior = self.SendSensorData(period=period)
         self.add_behaviour(behavior)
+        self.client = mqtt_client.Client()
+        self.client.on_connect = sensorSubscriber.on_connect
+        self.client.on_message = on_message
+        self.client.connect(BROKER, PORT)
+        self.client.subscribe(TOPIC)
--- peakDemo/sensorsPublisher.py
+++ peakDemo/sensorsPublisher.py
@@ -13,28 +13,27 @@ port = 1883
 client_id_base = 'publish-'
 # username = 'emqx'
 # password = 'public'
-
-class MQTTPublisher:
-    def __init__(self, client_id, topic):
-        self.client_id = client_id
-        self.topic = topic
-
-    def connect_mqtt(self):
+def connect_mqtt(client_id):
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
                 print("Connected to MQTT Broker!")
             else:
                 print("Failed to connect, return code %d\n", rc)
 
-        client = mqtt_client.Client(self.client_id)
+        client = mqtt_client.Client(client_id)
         # client.username_pw_set(username, password)
         client.on_connect = on_connect
         client.connect(broker, port)
         return client
 
+class MQTTPublisher:
+    def __init__(self, client_id, topic):
+        self.client_id = client_id
+        self.topic = topic
+
     def publish(self, client):
         #comentar esta linha
-        msg_count = 1
+        # msg_count = 1
         while True:
             time.sleep(1)
             report = str(random.randint(1000,5000))
@@ -47,13 +46,11 @@ class MQTTPublisher:
             else:
                 print(f"Failed to send message to topic {self.topic} using client ID: {self.client_id}")
             #comentar esta zona
-            msg_count += 1
-            if msg_count > 5:
-                break
+            # msg_count += 1
+            # if msg_count > 5:
+            #     break
 
-    def run(self):
-        client = self.connect_mqtt()
-        client.loop_start()
+    def run(self, client):
         self.publish(client)
         client.loop_stop()
 
@@ -61,14 +58,17 @@ class MQTTPublisher:
 if __name__ == '__main__':
     publishers = []
     topics = ["report/agent1", "report/agent2", "report/agent3", "report/agent4", "report/agentC"]
+    client_id = client_id_base + str(random.randint(0, 1000))
+    client = connect_mqtt(client_id)
+    client.loop_start()
+
     for topic in topics:
-        client_id = client_id_base + str(random.randint(0, 1000))
         publisher = MQTTPublisher(client_id, topic)
         publishers.append(publisher)
 
     threads = []
     for publisher in publishers:
-        thread = Thread(target=publisher.run)
+        thread = Thread(target=publisher.run, args=(client,))
         thread.start()
         threads.append(thread)
 
